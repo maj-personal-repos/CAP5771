bd4edb318644b98bc51fc01e32bd4a953f462cc1
Miguel Alonso Jr <drmiguelalonsojr@gmail.com>
2017-12-04 14:00:28 -0500

    more tweaks to tensorboard step2

--- Code/9 - Deep Learning/ex2 - tensorboard step2.py
+++ Code/9 - Deep Learning/ex2 - tensorboard step2.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 
+# example from tensorflow dev summit 2017 dandelion mane
+
 LOGDIR = "/tmp/mnist_tutorial/"
 mnist = tf.contrib.learn.datasets.mnist.read_data_sets(train_dir=LOGDIR + "data", one_hot=True)
 
@@ -7,20 +9,26 @@ mnist = tf.contrib.learn.datasets.mnist.read_data_sets(train_dir=LOGDIR + "data"
 # Define a simple convolutional layer
 def conv_layer(input, channels_in, channels_out, name="conv"):
     with tf.name_scope(name):
-        w = tf.Variable(tf.zeros([5, 5, channels_in, channels_out]))
-        b = tf.Variable(tf.zeros([channels_out]))
+        w = tf.Variable(tf.truncated_normal([5, 5, channels_in, channels_out], stddev=0.1), name="W")
+        b = tf.Variable(tf.constant(0.1, shape=[channels_out]), name="B")
         conv = tf.nn.conv2d(input, w, strides=[1, 1, 1, 1], padding="SAME")
         act = tf.nn.relu(conv + b)
+        tf.summary.histogram("weights", w)
+        tf.summary.histogram("biases", b)
+        tf.summary.histogram("activations", act)
         return tf.nn.max_pool(act, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME")
 
 
 # Define a fully connected layer
 def fc_layer(input, channels_in, channels_out, name="fc"):
     with tf.name_scope(name):
-        w = tf.Variable(tf.zeros([channels_in, channels_out]))
-        b = tf.Variable(tf.zeros([channels_out]))
-        act = tf.nn.relu(tf.matmul(input, w) + b)
-        return tf.nn.relu(tf.matmul(input, w) + b)
+        w = tf.Variable(tf.truncated_normal([channels_in, channels_out], stddev=0.1, name="W"))
+        b = tf.Variable(tf.constant(0.1, shape=[channels_out]), name="B")
+        act = tf.matmul(input, w) + b
+        tf.summary.histogram("weights", w)
+        tf.summary.histogram("biases", b)
+        tf.summary.histogram("activations", act)
+        return act
 
 
 # Setup placeholders, and reshape the data
@@ -30,6 +38,7 @@ def fc_layer(input, channels_in, channels_out, name="fc"):
 x = tf.placeholder(tf.float32, shape=[None, 784], name="x")
 y = tf.placeholder(tf.float32, shape=[None, 10], name="labels")
 x_image = tf.reshape(x, [-1, 28, 28, 1])
+tf.summary.image("input", x_image, 3)
 
 # create the network
 conv1 = conv_layer(x_image, 1, 32, "conv1")
@@ -41,32 +50,38 @@ logits = fc_layer(fc1, 1024, 10, "fc2")
 
 # Compute cross entropy as the loss function
 with tf.name_scope("xent"):
-    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=y))
+    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=y), name="xent")
+    tf.summary.scalar("xent", cross_entropy)
 
 # Use an AdamOptimizer to train the network
 with tf.name_scope("train"):
-    train_step = tf.train.AdadeltaOptimizer(1e-4).minimize(cross_entropy)
+    train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 
 # Compute the accuracy
 with tf.name_scope("accuracy"):
-    correct_predition = tf.equal(tf.arg_max(logits, 1), tf.arg_max(y, 1))
-    accuracy = tf.reduce_mean(tf.cast(correct_predition, tf.float32))
+    correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(y, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    tf.summary.scalar("accuracy", accuracy)
 
 # Initialize the variables
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
-# Train for 2000 steps
-for i in range(2000):
+merged_summary = tf.summary.merge_all()
+writer = tf.summary.FileWriter("/tmp/mnist_demo/3")
+writer.add_graph(sess.graph)
+
+# Train for 2001 steps
+for i in range(2001):
     batch = mnist.train.next_batch(100)
 
     # Occasionally report accuracy
-    if i % 500 == 0:
-        [train_accuracy] = sess.run([accuracy], feed_dict={x: batch[0], y: batch[1]})
+    if i % 5 == 0:
+        [train_accuracy, s] = sess.run([accuracy, merged_summary], feed_dict={x: batch[0], y: batch[1]})
+        writer.add_summary(s, i)
         print("step %d, training accuracy %g" % (i, train_accuracy))
 
     # Run the training step
     sess.run(train_step, feed_dict={x: batch[0], y: batch[1]})
 
-writer = tf.summary.FileWriter("/tmp/mnist_demo/1")
-writer.add_graph(sess.graph)
+
